78fe45f7a7693d0a6ca0ca5c9626ee27e6780ef8
allAboutJS <victoronah1914@gmail.com>
2025-04-03 08:13:44 +0100

    Rebuild project with react

--- server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import path from "node:path";
-
-const app = express();
-const port = process.env.PORT || "3000";
-
-app.use(express.static("./src"));
-app.listen(port, () => console.log("Server started."));
